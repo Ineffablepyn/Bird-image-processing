bcddbc0b6eb66c383f5f8d9e6f1e1d4af57634b8
Ineffablepyn <157698994+Ineffablepyn@users.noreply.github.com>
2024-12-25 03:40:23 +0800

    Update piecewise_Linear_Transform.m
    优化效果

--- piecewise_Linear_Transform.m
+++ piecewise_Linear_Transform.m
@@ -1,19 +1,23 @@
 function NewImage = piecewise_Linear_Transform(Image, a, b, c, d)
-    % 获取图像尺寸
     [h, w] = size(Image);
-    
-    % 初始化输出图像
     NewImage = zeros(h, w, 'double');
-    
+
+    output_max = min(d, 230); % 将上限设为230以减少高光
+
     % 三段线性灰度级变换
     for x = 1:w
         for y = 1:h
             if Image(y,x) < a
-                NewImage(y,x) = Image(y,x) * c/a;
+                NewImage(y,x) = Image(y,x) * (c / a);  % 线性缩放
             elseif Image(y,x) < b
-                NewImage(y,x) = (Image(y,x) - a) * (d - c) / (b - a) + c;
+                NewImage(y,x) = (Image(y,x) - a) * (output_max - c) / (b - a) + c; % 线性映射
             else
-                NewImage(y,x) = (Image(y,x) - b) * (1 - d) / (1 - b) + d;
+                NewImage(y,x) = (Image(y,x) - b) * ((output_max - d) / (255 - b)) + output_max; 
             end
         end
     end
+    
+    NewImage = min(max(NewImage, 0), 255);
+    
+    NewImage = uint8(NewImage);
+end
